a92343aea6a302eee398f128208f7965ff1e76a5
CregskiN <272473132@qq.com>
2022-12-06 16:18:04 +0800

    完成1.7_条件渲染

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,68 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+function LoginButton(props: any) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props: any) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+function Hello(props: any) {
+
+  if (!props.isLoggedIn) { // 阻止渲染 // TODO：该阻止渲染的方法，是否会调用 ComponentWillUnmount？从文档来看，不会
+    return null;
+  }
+  return <h1>hello, {props.isLoggedIn ? "has Logged in" : "has not logged in"}</h1>
+}
+
+class LoginControl extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Hello isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <LoginControl />
 );
 
 // If you want to start measuring performance in your app, pass a function
