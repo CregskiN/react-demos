a655bdc546e58c906941055f6dbcebbe3215ba94
CregskiN <272473132@qq.com>
2022-12-06 08:32:40 +0800

    完成1.2_JSX简介

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,55 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { userInfo } from 'os';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+/**
+ * 在 JSX 中嵌入表达式 用 { }
+ */
+// interface IUser {
+//   firstName: string;
+//   lastName: string;
+// }
+// function formatName(user: IUser) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+// const user: IUser = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+
+// const element = <h1>Hello {formatName(user)}!</h1>;
+
+/**
+ *  在 JSX 中给标签属性赋值用 ‘ ’
+ */
+
+/**
+ * JSX 防止 XSS
+ */
+// const XSS = `<script> alert('XSS'注入) </scrpit>`;
+// const element = <div>{XSS}</div>
+
+
+/**
+ * Babel 将 JSX 转义
+ */
+const elementJSX = <h1 className='greeting'>Hello World!</h1>
+const elementBabel = React.createElement(
+  'h1',
+  { className: 'greeting' },
+  'Hello Worlds!'
+);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    {elementJSX}
+    {elementBabel}
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
