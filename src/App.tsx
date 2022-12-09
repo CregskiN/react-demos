15e52c3d72528c05cfc06d12373006e0be712aff
CregskiN <272473132@qq.com>
2022-12-09 10:30:22 +0800

    完成2.9_深入JSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * 深入 JSX
+ * 1. 指定 React 元素类型（使用 React HTML 标签）
+ *  必须在作用域内。使用 JSX 必须有 import React 或将 React 挂载到全局变量
+ * 2. JSX 类型必须是大写开头
+ * 3. prop 如果没赋值，默认为 true
+ * 4. render() return [<li></li>, <li></li>, <li></li>, <li></li>]
+ * 5. false, null, undefined, and true 是合法的子元素。但它们并不会被渲染
+ */
+
 function App() {
   return (
     <div className="App">
